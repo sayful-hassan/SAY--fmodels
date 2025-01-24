3049440b1a4ce5c494b6e1a8d2adf49ba04185c9
sayful-hassan <sayf.fashion@gmail.com>
2025-01-24 19:06:47 +0530

    Basic header is created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Header() {
+  return (
+        <div className="container-fluid">
+            <div className="row">
+                <header className="col-md-12">
+                    <h1>SAY FMODEL</h1>
+                    <nav>
+                        <ul className="col-md-8">
+                            <li>HOME</li>
+                            <li>MODELS</li>
+                            <li>ABOUT</li>
+                            <li>CONNECT</li>
+                        </ul>
+                    </nav>
+                </header>    
+            </div>
+        </div>
+  )
+}
+
+export default Header
