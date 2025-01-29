34543d03cbc1d7e32e44aeb7ed351a11fbb28988
sayful-hassan <sayf.fashion@gmail.com>
2025-01-29 15:19:00 +0530

    About Page is created both path and content, also context page button alignment is cleared

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Models from './components/Models/Models'
 import Header from './components/Header/Header'
 import Context from './components/Context/Context'
+import About from './components/About/About'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gigi from './components/Models/List of Models/Gigi'
 import Kylie from './components/Models/List of Models/Kylie'
@@ -19,6 +20,7 @@ function App() {
        <Routes>
         <Route path='/' element={<Context/>} />
         <Route path='/models' element={<Models/>} />
+        <Route path='/about' element={<About/>} />
         <Route path='/models/gigi' element={<Gigi/>} />
         <Route path='/models/kylie' element={<Kylie/>} />
         <Route path='/models/kendall' element={<Kendall/>} />
---
+++ src/components/About/About.jsx
@@ -0,0 +1,25 @@
+import "./About.css";
+
+function About() {
+  return (
+    <>
+      <p>
+        Welcome to our modeling agency, where talent meets opportunity. Our
+        platform provides a space for aspiring models to showcase their profiles
+        and connect with companies looking for fresh talent. Companies can post
+        their requirements for various product campaigns, and talented
+        individuals can easily find work through our platform. For those who may
+        be hesitant to market themselves, our dedicated team is here to offer
+        personalized guidance and support, helping you navigate the industry
+        with confidence. This platform also streamlines the process for clients,
+        eliminating the need to search through social media for trending
+        influencers. Instead, clients can search, filter, and discover the
+        perfect model for their campaigns, while also posting specific
+        requirements—all in one place. Here, both talent and opportunity come
+        together seamlessly.
+      </p>
+    </>
+  );
+}
+
+export default About;
